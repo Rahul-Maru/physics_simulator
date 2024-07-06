0f147a9413bf5dd25c4f300caf9302add9b6829a
Rahul Maru <rahulmaru3507@gmail.com>
2024-07-06 17:30:27 +0530

    integrate vector class into code

--- physim.py
+++ physim.py
@@ -5,6 +5,7 @@ from matplotlib import pyplot as plt
 import math
 from math import pi, e, sqrt, sin, cos, tan
 import pygame as pg
+from vector import Vector
 from particle import Particle
 
 WINDOW_WIDTH = 500 # pixels
@@ -14,15 +15,17 @@ FRAMERATE = 60 # frames/s
 
 spring = pg.image.load("spring.png") # source: khanacademy
 
-g = -9.8 # m/s^2
-k = 5 # N/m
+g = -9.8 # m/s^2  gravity on earth
+k = 3 # N/m  spring constant
+b = 0.1 # Ns/m
 m = 0.4 # kg
 yr = -2 # m
 WIDTH = LEN_SCALE
 
 
-Fs = lambda s : (0, -k*(s[1]-yr))
-Fg = lambda : (0, g*m)
+Fs = lambda s : Vector(0, -k*(s.comps[1]-yr))
+Fd = lambda v : -v*b
+Fg = lambda : Vector(0, g*m)
 
 def main():
 	pg.init()  
@@ -30,29 +33,31 @@ def main():
 	clock = pg.time.Clock()
 	done = False
 
-	s0 = ((WINDOW_WIDTH-WIDTH)/(2*LEN_SCALE), -1.25)
-	u = (0, 0)
+	s0 = Vector((WINDOW_WIDTH-WIDTH)/(2*LEN_SCALE), -0.5)
+	u = Vector(0, 0)
 
 	t = 0
+	update_t = 0
 	s = s0
-	F = Fs(s0)
-	a = (F[0]/m, F[1]/m + Fg()[1])
-
+	a = (Fs(s0) + Fg())/m
 	# v(1/2 * dt) | setup for the leap-frog integration method
-	v = (u[0] + a[0] * 1/(2*FRAMERATE), u[1] + a[1] * 1/(2*FRAMERATE))
+	v = u + a * 1/(2*FRAMERATE)
 	  
 	while not done:
 		dt = clock.tick_busy_loop(FRAMERATE)/1000  # s
 		t += dt # s
 
-		s = (s[0] + v[0]*dt, s[1] + v[1]*dt)
+		s += v*dt
 
-		fs = Fs(s)
-		fg = Fg()
-		F = (fs[0] + fg[0], fs[1] + fg[1])
-		a = (F[0]/m, F[1]/m)
+		F = Fs(s) + Fg() + Fd(v)
+		a = F/m
 
-		v = (v[0] + a[0]*dt, v[1] + a[1]*dt)
+		v = v + a*dt
+
+		if t-1 > update_t:
+			update_t += 1
+			print(f"{update_t} ||| {s} || {v} || {a}")
+
 
 		for event in pg.event.get():
 			if event.type == pg.QUIT:
@@ -60,10 +65,10 @@ def main():
 		
 		screen.fill((0, 0, 0))
 
-		screen.blit(pg.transform.scale(spring, (LEN_SCALE, max(-s[1]*LEN_SCALE, 0))), (s0[0]*LEN_SCALE, 0))
+		screen.blit(pg.transform.scale(spring, (LEN_SCALE, max(-s.comps[1]*LEN_SCALE, 0))), (s0.comps[0]*LEN_SCALE, 0))
 
-		pg.draw.rect(screen, (128, 255, 0), pg.Rect(s0[0]*LEN_SCALE, -s[1]*LEN_SCALE, WIDTH, LEN_SCALE/9))
-		pg.draw.line(screen, (255, 80, 0), (2.01*LEN_SCALE, -s[1]*LEN_SCALE), (2.99*LEN_SCALE, - s[1]*LEN_SCALE), 2)
+		pg.draw.rect(screen, (128, 255, 0), pg.Rect(s0.comps[0]*LEN_SCALE, -s.comps[1]*LEN_SCALE, WIDTH, LEN_SCALE/9))
+		pg.draw.line(screen, (255, 80, 0), (2.01*LEN_SCALE, -s.comps[1]*LEN_SCALE), (2.99*LEN_SCALE, - s.comps[1]*LEN_SCALE), 2)
 		pg.draw.line(screen, (200, 200, 200), (1.5*LEN_SCALE, -yr*LEN_SCALE), (3.5*LEN_SCALE, - yr*LEN_SCALE))
 
 		fps = clock.get_fps()
