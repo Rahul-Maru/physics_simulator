a8786444e84254fbabf8bf79e5744a0cf6f48995
Rahul Maru <rahulmaru3507@gmail.com>
2024-07-10 13:31:43 +0530

    add spring.py, consts.py create draw function. crop spring.png

--- src/particle.py
+++ src/particle.py
@@ -1,36 +1,47 @@
 from __future__ import annotations
 from numbers import Number
 from vector import Vector
+from consts import pg, SCALE, WIDTH
 import math
 
 class Particle:
-	def __init__(self, m: Number, q: Number, s, u, l=0, w=0) -> None:
-		self.m = m; self.q = q; self.l = l; self.w = w
-		if isinstance(s, Vector):
-			self.s = s
+	def __init__(self, m: Number, q: Number, s, u, l=0.4, w=0.4, color: tuple = (128, 255, 0)) -> None:
+		self.m = m # kg  mass
+		self.q = q # C  charge
+		self.l = l # m  length
+		self.w = w # m  width
+		self.color = color
 
+		if isinstance(s, Vector):
+			self.s = s # m
 		else:
-			self.s = Vector(s)
+			self.s = Vector(s) # m
 
 		if isinstance(u, Vector):
-			self.v = u
+			self.v = u # m/s
 		else:
-			self.v = Vector(u)
+			self.v = Vector(u) # m/s
 	
 	def move(self, F: Vector, dt: Number, Ufn = lambda : None) -> None:
-		a = F/self.m
+		a = F/self.m # m/s²
 
+		# update the velocity to be on a whole-numbered time step
 		self.v += a*dt/2
+		# calculate energy when s, and v are lined up
 		if Ufn:
 			U = Ufn(self.s, self.v)
 		else: 
 			U = 0
+		# update velocity back to half-step to continue leap-frogging
 		self.v += a*dt/2
 
 		self.s += self.v*dt
 
-
-		return U
+		return U # J
 	
+	def draw(self, screen: pg.Surface) -> None:
+		pg.draw.rect(screen, self.color, pg.Rect(self.s.x()*SCALE, -self.s.y()*SCALE, self.l*SCALE, self.w*SCALE))
+
+
 	def __str__(self) -> str:
 		return f"m: {self.m} || q: {'+' if self.q > 0 else ''}{self.q} || s: {self.s} v: {self.v}"
--- src/physim.py
+++ src/physim.py
@@ -1,83 +1,61 @@
-import vpython as vp
-import numpy as np
+from consts import *
 
-from matplotlib import pyplot as plt
-import math
 from math import pi, e, sqrt, sin, cos, tan
-import pygame as pg
 from vector import Vector
 from particle import Particle
-
-WINDOW_WIDTH = 500 # pixels
-WINDOW_HEIGHT = 500 # pixels
-LEN_SCALE = 100 # pixel/m
-FPS = 41 # frames/s
-
-clock = pg.time.Clock()
-
-spring = pg.image.load("imgs/spring.png") # source: khanacademy
-
-g = -9.8 # m/s^2  gravitational constant
-k = 8 # N/m  spring constant
-b = 0 # Ns/m
-m = 0.6 # kg
-yr = -2 # m
-WIDTH = LEN_SCALE # m  width of spring
-
-Fs = lambda s : Vector(0, -k*(s.y()-yr))
-Fd = lambda v : -v*b
-Fg = lambda : Vector(0, g*m)
-
-s0 = Vector((WINDOW_WIDTH-WIDTH)/(2*LEN_SCALE), -1) # m
-a0 = (Fs(s0) + Fg())/m # m/2^2
-u = Vector(0, 0) # m/s
-
+from spring import Spring
 
 def main():
-	pg.init()  
-	screen = pg.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
+	pg.init()
 	done = False
+
+	t = log_t = 0
+
+	spg = Spring(k, Vector(s0.x(), 0), sr, WIDTH)
+
+	# functions for net force and energy
+	F = lambda s, v : spg.F(s) + Fg() + Fd(v) # N
+	E = lambda s, v: -m*g*s.y() + m*v.mag()**2/2 + spg.energy(s) # J
+
+	a0 = (F(s0, u))/m # m/s²
 
 	# v(dt/2) | setup for the leap-frog integration method
 	v05 = u + a0/(2*FPS) # m/s
 
-	p = Particle(m, 0, s0 + v05/FPS, v05)
+	p = Particle(m, 0, s0 + v05/FPS, v05, WIDTH/SCALE, 1/11)
 
-	t = 0
-	update_t = 0
-	  
+	# simulator loop
 	while not done:
 		dt = clock.tick_busy_loop(FPS)/1000  # s
 		t += dt # s
 
-		F = Fs(p.s) + Fg() + Fd(p.v) # N
-		U = p.move(F, dt, lambda s, v: -m*g*s.y() + (m*v.mag()**2 + k*(s.y() - yr)**2)/2) # J
+		F_net = F(p.s, p.v) # N
+		U = p.move(F_net, dt, E) # J
 
-		if t-1 > update_t:
-			update_t += 1
-			print(f"{update_t} ||| {p.s.y()} || {p.v.y()} || {F.y()/m} || {U}")
+		spg.s = p.s
+
+		if t > log_t + 1:
+			log_t += 1
+			print(f"{log_t} ||| {p.s} || {p.v} \n {F_net/m} || {U}")
 
 
 		done = handle_events()
 
-		draw(screen, [p], t)
-		
+		draw(screen, [spg, p], t)
+
+
 def handle_events():
 	for event in pg.event.get():
 			if event.type == pg.QUIT:
 				return True
 	return False
 
-def draw(screen, ps, t):
+def draw(screen, objs, t):
 	screen.fill((0, 0, 0))
 
+	for obj in objs:
+		obj.draw(screen)
 
-	for p in ps:
-		screen.blit(pg.transform.scale(spring, (LEN_SCALE, max(-p.s.comps[1]*LEN_SCALE, 0))), (s0.comps[0]*LEN_SCALE, 0))
-		pg.draw.rect(screen, (128, 255, 0), pg.Rect(s0.comps[0]*LEN_SCALE, -p.s.comps[1]*LEN_SCALE, WIDTH, LEN_SCALE/9))
-		pg.draw.line(screen, (255, 80, 0), (2.01*LEN_SCALE, -p.s.comps[1]*LEN_SCALE), (2.99*LEN_SCALE, - p.s.comps[1]*LEN_SCALE), 2)
-
-	pg.draw.line(screen, (200, 200, 200), (1.5*LEN_SCALE, -yr*LEN_SCALE), (3.5*LEN_SCALE, - yr*LEN_SCALE))
 
 	fps = clock.get_fps()
 	font = pg.font.SysFont(None, 14)
@@ -89,6 +67,7 @@ def draw(screen, ps, t):
 
 
 	pg.display.flip()
+
 
 if __name__ == "__main__":
 	main()
