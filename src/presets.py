3b80f56202b2b637ee3dee5577985131d23d9433
Rahul Maru <rahulmaru3507@gmail.com>
2024-12-09 11:25:27 +0530

    add polar projection of moon's position as seen from earth

--- src/presets.py
+++ src/presets.py
@@ -10,16 +10,12 @@ def preset(path: str):
 	with open(f"presets/{path}.json") as f:
 		dat = json.load(f)
 		dat['center'] = OBJECT_MAP.index(dat['center'])
-		dat['ndims'] = len(list(filter(lambda x:x, dat['dims'])))
+		dat['ndims'] = 2 if dat['mode'] == 'polar' else len(list(filter(lambda x:x, dat['dims'])))
 
-		temp = {k: SIZE_MAP.index(dim) for k, dim in zip(['x', 'y', 'z'], dat['dims']) if dim}
-		# for n, dim in zip(['x', 'y', 'z'], dat['dims']):
-		# 	if dim:
-		# 		temp.append(SIZE_MAP.index(dim)) 
-		
-		dat['dims'] = temp
-		
+		if 'dims' in dat:
+			dat['dims']  = {k: SIZE_MAP.index(dim) for k, dim in zip(['x', 'y', 'z'], dat['dims']) if dim}
+
 		return dat
 
 if __name__ == "__main__":
-	print(preset("earth-moon-xy"))
+	print(preset("moon-pov"))
